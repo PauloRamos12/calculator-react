454efc83d980d8aea9666860318842c34d65de0c
PauloRamos12 <pramosphdr548@gmail.com>
2023-05-11 11:44:05 -0300

    Project Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [history, setHistory] = useState([]);
+
+  const handleButtonClick = (value) => {
+    setInput((prevInput) => prevInput + value);
+  };
+
+  const handleClear = () => {
+    setInput('');
+  };
+
+  const handleEquals = () => {
+    try {
+      const result = eval(input);
+      setInput(result.toString());
+      setHistory([...history, { expression: input, result }]);
+    } catch (error) {
+      alert('Erro: Expressão inválida!');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <div className="display">{input}</div>
+      <div className="button-row">
+        <button className="button" onClick={() => handleButtonClick('7')}>7</button>
+        <button className="button" onClick={() => handleButtonClick('8')}>8</button>
+        <button className="button" onClick={() => handleButtonClick('9')}>9</button>
+        <button className="button operator" onClick={() => handleButtonClick('*')}>×</button>
+      </div>
+      <div className="button-row">
+        <button className="button" onClick={() => handleButtonClick('4')}>4</button>
+        <button className="button" onClick={() => handleButtonClick('5')}>5</button>
+        <button className="button" onClick={() => handleButtonClick('6')}>6</button>
+        <button className="button operator" onClick={() => handleButtonClick('-')}>−</button>
+      </div>
+      <div className="button-row">
+        <button className="button" onClick={() => handleButtonClick('1')}>1</button>
+        <button className="button" onClick={() => handleButtonClick('2')}>2</button>
+        <button className="button" onClick={() => handleButtonClick('3')}>3</button>
+        <button className="button operator" onClick={() => handleButtonClick('+')}>+</button>
+      </div>
+      <div className="button-row">
+        <button className="button" onClick={() => handleButtonClick('0')}>0</button>
+        <button className="button" onClick={() => handleButtonClick('.')}>.</button>
+        <button className="button operator" onClick={() => handleEquals()}>=</button>
+        <button className="button operator" onClick={() => handleButtonClick('/')}>÷</button>
+      </div>
+      <div className="button-row">
+        <button className="button clear" onClick={() => handleClear()}>C</button>
+      </div>
+      {history.length > 0 && (
+        <div className="history">
+          <h2>Histórico de Cálculos:</h2>
+          <ul>
+            {history.map((item, index) => (
+              <li key={index}>
+                {item.expression} = {item.result}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
